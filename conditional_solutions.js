9ee634948228f0b913f08cf05eb5ea6e0a2b0973
Nicolle Loayza <nicolleloayza@Nicolles-MacBook-Pro.local>
2018-10-04 18:43:09 -0400

    Conditional Solutions

---
+++ conditional_solutions.js
@@ -0,0 +1,109 @@
+/*
+1.
+let a = 1
+let b = 1
+
+if (a === b) {
+  console.log('Equal');
+}
+else {
+  console.log('Not equal')
+}
+*/
+
+/*2.
+
+let x = 5;
+let y = 10;
+
+if (x > y) {
+  console.log('The greater number of ' + x + ' and ' + y + ' is ' + x)
+} else if (y > x) {
+    console.log(' The greater number of ' + x + ' and ' + y + ' is ' + y)
+  } else {
+    console.log(' The numbers are equal ')
+  }
+*/
+
+/*3.
+var language = 'English'
+
+if (language === 'French') {
+  console.log('Bonjour monde')
+} else if (language === 'Spanish') {
+  console.log('Hola Mundo')
+} else if (language === 'Japanese') {
+  console.log('Konnichiwa sekai')
+} else {
+  console.log('Hello World')
+}
+*/
+
+/*4.
+var language = 'English';
+switch (language){
+  case 'French':
+    console.log('Bonjour monde');
+    break;
+  case 'Spanish':
+    console.log('Hola Mundo');
+    break;
+  case 'Japanese':
+  console.log('Konnichiwa sekai');
+  break;
+  default:
+    console.log('Hello World');
+    break;
+  }
+  */
+
+/*5.
+let numGrade = 92
+
+if (numGrade < 55) {
+  console.log('F')
+} else if (numGrade < 65) {
+  console.log('D')
+} else if (numGrade < 75) {
+  console.log('C')
+} else if (numGrade < 85) {
+  console.log('B')
+} else {
+  console.log('A')
+}
+*/
+
+/*6.
+var animalName = "cat"
+var numberOfAnimals = 2
+
+if (numberOfAnimals === 1) {
+  console.log(numberofAnimals + '' + animalName)
+}else {
+  console.log(numberOfAnimals + '' + animalName + 's')
+}
+*/
+
+/*7.
+let num = 2
+if (num % 2 === 0) {
+  console.log('Even')
+}else{
+console.log('Odd')
+}
+*/
+
+/*8.
+var num = 4
+num % 2 === 0 ? console.log('Even') : console.log('Odd')
+*/
+
+//9.
+num = 10
+
+if (num > 0) {
+   console.log("Positive number")
+ } else if (num === 0) {
+   console.log("Equal to 0")
+} else {
+   console.log("Negative number") }
