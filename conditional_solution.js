c5d9a123c07d56fb9c275d793eecb58ae7c6696c
godsby <gg4biz@gmail.com>
2018-10-04 18:35:16 -0400

    assignment_conditional

---
+++ conditional_solution.js
@@ -0,0 +1,104 @@
+//Quention 1
+/*
+let color1 = "red";
+let color2 = "yellow";
+if (color1 === color2) {
+  console.log ("They are equal colors!")
+} else {
+  console.log ("They are not equal colors.")
+}
+
+//Question 2
+
+let num1 = 9;
+let num2 = 8;
+if (num1 > num2) {
+  console.log("The greater number of " + num1 + " and " + num2 + " is " + num1);
+} else if (num1 < num2) {
+  console.log("The greater number of " + num1 + " and " + num2 + " is " + num2)
+} else {
+  console.log("The numbers are equal.")
+}
+
+//Question 3
+
+let language = "French";
+if (language === "Spanish") {
+  console.log("Hola Mundo!");
+} else if (language === "German") {
+  console.log("Hallo Welt!");
+} else if (language === "French"){
+  console.log("Bonjour le monde!")
+} else {
+  console.log("Hello World!");
+}
+
+
+//Question 4
+
+let language = "French";
+switch (language) {
+  case "Spanish":
+    console.log("Hola Mundo!");
+    break;
+  case "German":
+    console.log("Hallo Welt!")
+    break;
+  case "French":
+    console.log("Bonjour le monde!")
+    break;
+  default:
+    console.log("Hello World!")
+}
+
+//Question 5
+
+let numGrade = Math.ceil(Math.random() * 100);
+if (numGrade >= 90) {
+  console.log("A");
+} else if (numGrade >= 80) {
+  console.log("B");
+} else if (numGrade >= 70) {
+  console.log("C");
+} else if (numGrade >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
+
+//Question 6
+
+let animalName = "sheep";
+let numberOfAnimals = 2;
+if (numberOfAnimals > 1) {
+  console.log(numberOfAnimals + " " + animalName + "s.")
+} else {
+  console.log(numberOfAnimals + " " + animalName + ".");
+}
+
+//Quention 7
+
+let i = 6;
+if (i % 2 === 0) {
+  console.log(i + " is an even number.");
+} else {
+  console.log(i + " is an odd number.");
+}
+
+//Question 8
+
+let i = 6;
+let evenOrOdd = (i % 2 === 0) ? i + " is an even number." : i + " is an odd number.";
+console.log(evenOrOdd);
+
+//Question 9
+
+let i = -5;
+if (i > 0) {
+  console.log(i + " is a positive number.");
+} else if (i < 0) {
+  console.log(i + " is a negative number.");
+} else {
+  console.log(i + " is equal to 0.");
+}
+*/
