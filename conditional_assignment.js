ac031e50276fe5bef58d73328afe41fa79e2baa9
jungraejang <jungraejang@pursuit.org>
2018-10-04 21:47:30 -0400

    conditional_assignment

---
+++ conditional_assignment.js
@@ -0,0 +1,113 @@
+Question 1
+// let num1 = 9
+// let num2 = 6
+//
+// if(num1 === num2){
+//   console.log('Two numbers are the same')
+// } else {
+//   console.log('Two numbers are different')
+// }
+//////////////////////////////
+Question 2
+// let x = 8
+// let y = 14
+//
+// if(x > y){
+//   console.log('The greater number of ' + x + ' and ' + y + ' is ' + x)
+// } if(x < y){
+//   console.log('The greater number of ' + y + ' and ' + x + ' is ' + y)
+// } if(x === y) {
+//   console.log('The numbers are equal')
+// }
+//////////////////////////////
+Question 3
+// let mainLang = ''
+//
+// if(mainLang === 'french'){
+//   console.log('Bonjour!')
+// } if(mainLang === 'german'){
+//   console.log('Gutten Tak!')
+// } else if(mainLang === 'spanish'){
+//   console.log('Hola!')
+// } else {
+//   console.log('Hello!')
+// }
+///////////////////////////////////////
+Question 4
+// let mainLang = ''
+//
+// switch (mainLang) {
+// case 'french':
+//   console.log('Bonjour!');
+//   break;
+// case 'german':
+//   console.log('Gutten Tak!');
+//   break;
+// case 'spanish':
+//   console.log('Hola!');
+//   break;
+// default:
+//   console.log('Hello!')
+//   break;
+// }
+///////////////////////////////////////
+Question 5
+// let grade = 97
+//
+// if(grade >= 90 && grade <= 100){
+//   console.log('A')
+// } if(grade >= 80 && grade <= 89){
+//   console.log('B')
+// } if(grade >= 70 && grade <= 79){
+//   console.log('C')
+// } if(grade >= 60 && grade <= 69){
+//   console.log('D')
+// } else if(grade >= 0 && grade <= 59) {
+//   console.log('F')
+// }
+//////////////////////////////////////
+// Question 6
+// let animalName = "cat"
+// let numberOfAnimals = 34
+//
+// if(numberOfAnimals === 1){
+//   console.log(animalName)
+// } else if(numberOfAnimals >= 2){
+//   console.log(animalName + "s")
+} /////////////////////////////////////
+Bonus Question
+// let animalName = "dog"
+// let numberOfAnimals = 8
+//
+// if(numberOfAnimals <= 1){
+//   console.log(animalName)
+// } else if(numberOfAnimals >= 2 && animalName === "goose"){
+//   console.log(numberOfAnimals + " " + " Geese")
+// } else if(numberOfAnimals >= 2 && animalName === "sheep"){
+//   console.log(numberOfAnimals + " Sheep")
+// } else if(numberOfAnimals >= 2){
+//   console.log(numberOfAnimals + " " + animalName + "s")
+// }
+Question 7
+// let randomNumber = 234242342342
+//
+// if(randomNumber % 2 === 0){
+//   console.log('Even')
+// } else if(randomNumber % 2 !== 0){
+//   console.log('Odd')
+// }
+// //////////////////////////////////////
+Question 8
+// let randomNumber = 25;
+// let oddOrEven = (randomNumber % 2 === 0) ? "Even" : "Odd";
+// console.log(oddOrEven);
+////////////////////////////////////////
+Question 9
+// let randomNumber = 0
+// if(randomNumber === 0){
+//   console.log("Zero")
+// } else if(randomNumber < 0){
+//   console.log("Negative")
+// } else {
+//   console.log("Potitive")
+// }
