3df63042d726a60835256a2241d969a217672122
Andres.C <andrescabrera@pursuit.org>
2018-10-04 23:16:43 -0400

    conditional

---
+++ conditionalHW.js
@@ -0,0 +1,87 @@
+//1.
+// let num = 2
+// if ( num === 2){
+//   console.log('oooo yea')
+// }
+
+//2.
+// let num = 25
+// if (num < 100) {
+//   console.log('The greater number of 25 and 100 is 100')
+// }
+
+//3
+// let lang = "English"
+// if ( lang === "German" ){
+//   console.log ("Hallo Welt")
+// } else if  (lang === "Spanish"){
+//    console.log ("Hola Mundo")
+// } else if (lang === "Filipino") {
+//   console.log ( "kumusta sa mundo")
+// } else if  (lang === "English"){
+//   console.log ( "Hello world")
+// } else {
+//   console.log ( "English")
+//
+// }
+
+
+// 4
+// let lang = "English"
+// switch(lang){
+//      case "German":
+//       console.log ("Hallo Welt");
+//       break;
+//      case "Spanish":
+//      console.log ("Hola Mundo");
+//       break;
+//       case "Filipino":
+//     console.log ("kumusta sa Mundo");
+//       break;
+//       case "English":
+//     console.log ("Hello World");
+//       break;
+//       default: console.log ( "English it shall be!")
+//     }
+
+
+// 5
+// let numGrade = 45
+// if ( numGrade <= 100 && numGrade > 89 ){
+//   console.log ("You got a A")
+// } else if  (numGrade <= 89  && numGrade > 84 ){
+//    console.log ("You got a B")
+// } else if (numGrade <= 83 && numGrade > 75) {
+//   console.log ( "You got a C")
+// } else if  (numGrade <= 75 && numGrade > 69){
+//   console.log ( "You got a D")
+// } else if  (numGrade <= 69 && numGrade > 65){
+//   console.log ( "You got a E")
+// } else  {
+//   console.log ( "You got an F")
+// }
+
+
+
+// 6.
+
+// let numberOfAnimals = 10
+// let animalName = ('dog')
+// if (animalName === 'dog'){
+//   console.log (numberOfAnimals   + animalName + "s")
+// } else {
+//   console.log ("too many animals")
+// }
+
+// 7.
+// let num = 22121201329
+// if  (num % 2 === 0){
+//   console.log (num  + " is even")
+// }else{
+//   console.log (num  +  " is odd")
+// }
+
+// 8.
+// let num = 15
+// 15 % 2 == 0 ? "even" : "odd" ;
+//
