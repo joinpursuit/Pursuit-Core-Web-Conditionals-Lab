b2fe72abcf01ce21b133feea700889fae2d03436
karenm09 <karenm929@hotmail.com>
2019-06-30 18:25:47 -0400

    ansswer to conditional_exercise

---
+++ conditional_exercises.js
@@ -0,0 +1,99 @@
+//1. Write a block of code that checks and logs whether variables are equal or not.
+      // let num = 4
+      // if (num = 3){
+      //   console.log("It is equal");
+      // } else {
+      //   console.log("They are not equal");
+
+//2. Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
+//If the two numbers are equal, log: 'The numbers are equal.'*/
+    // let x = 3
+    // let y = 15
+    // if (x > y){
+    //   console.log(x + " is the greater value");
+    // } else if(x===y){
+    //   console.log("Both values are equal ");
+    // } else {
+    //   console.log(x + " is less than " + y)
+    // }
+//3. Create a string variable with a language code (e.g. "english", "spanish", "german").
+  // Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages.
+  //It should default to logging English.
+      // let language = "hebrew"
+      //   if (language === "spanish") {
+      //   console.log("Hola Mundo");
+      // } else if (language === "german") {
+      //   console.log("Hallo Welt");
+      // } else {
+      //   console.log("Hello World")
+      // }
+// 4. Do question 3 again but this time use a `switch` statement.
+    //   let lang = "spanish"
+    //   switch (lang) {
+    //     case "spanish":
+    //       console.log("Hola Mundo");
+    //     break;
+    //     case "german":
+    //       console.log("Hallo Welt");
+    //     break;
+    //     default:
+    //       console.log("Hello World")
+    // }
+// 5. Create a variable numGrade that will contain a numeric grade (out of 100).
+//Use an if - else if ... - else statement to log the corresponding letter grade, either "A", "B", "C", "D", or "F".
+     // let grade = 50
+     // if (grade>=90) {
+     //   console.log("A");
+     // } else if (grade>=80) {
+     //   console.log("B");
+     // } else if (grade>=70) {
+     //   console.log("C");
+     // } else if (grade>=65) {
+     //   console.log("D");
+     // } else {
+     //   console.log("F");
+     // }
+//6. Create two variables: animalName ('cat', 'dog', etc.) , and numberOfAnimals.
+
+//Use an if - else statement to log:
+
+// * the number
+
+//* either in single or plural form. Exp: "5 cats" or "1 dog".
+
+      // let animalName = "chicken"
+      // let numberOfAnimals = 1
+      //
+      // if ((animalName === animalName) && (numberOfAnimals === 1)){
+      //   console.log(numberOfAnimals +" " + animalName)
+      // } else {
+      //   console.log(numberOfAnimals + " " + animalName +"s");
+      //}
+//Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+// Still working on bonus
+   // let animalName = "dog"
+   // let numberOfAnimals = 3
+   //  if (numberOfAnimals > 1){
+   //    console.log(numberOfAnimals + " " + animalName +"s.")
+   //    if (numberOfAnimals > 1 && animalName === "goose")
+   //      console.log(numberOfAnimals + " geese")
+   //    else {
+   //    console.log(numberOfAnimals + " " +animalName)
+    }
+//7. Write a block of code that logs whether a given number is even or odd.
+      //let number = 14
+       // if (number%2===0){
+       //   console.log("Number is even")
+       // } else {
+       //   console.log("Number is odd")
+       // }
+ // 8. Do question 7 again but use a ternary.
+      //number%2===0 ? console.log("Number is even") : console.log("Number is odd");
+// 9. Write a block of code that checks and logs whether a given number is positive, negative, or equal to 0.
+    // let output = 0
+    // if (output>0) {
+    //   console.log(output + " is a positive number")
+    // } else if (output<0) {
+    //   console.log(output + " is a negative number")
+    // } else {
+    //   console.log("number is equal to 0")
