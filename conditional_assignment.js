01ea97545975d589ef4ece0e771a41c1d2bd9f77
Pearl <pearljeanpierre@pursuit.org>
2019-06-30 18:42:47 -0400

    conditional lab assignment

---
+++ conditional_assignment.js
@@ -0,0 +1,69 @@
+//let num = 2;
+//if (num === 6) {
+  //console. log("Not equal");
+//}
+
+//let x = 18;
+//let y = 10;
+//if (x > y){
+//console.log("The greater number of"+ " " + x + " " + "and" + " " + y + " "+ "is" + " "+ x);
+//}else if (y > x) {
+  //console.log ("The greater number of"+ " " + x + " " + "and" + " "+ y + " " + "is" + " "+ y)
+  //} else {
+    //console.log("Numbers are equal");
+//}
+//}
+
+//let phrase = "Hola Mundo";
+ //if (phrase === "Hallo Welt" || phrase === "Hello World"){
+ //console.log("Not Spanish");
+//} else if (phrase === "Hello Welt") {
+   //console.log("German");
+ //} else {
+   //console.log("English greeting")
+//}
+
+//switch ("Hello World") {
+//case("Hola Mundo"):
+//console.log("Spanish")
+//break;
+//case("Hallo Welt"):
+//console.log("German")
+//break;
+//default:("Hello World")
+//console.log("English")
+//}
+//let numGrade = 95;
+//if(numGrade >65 && numGrade <75){
+//console.log("You have a D");
+//} else if (numGrade <65) {
+//console.log("You have an F")
+//}else{
+  //console.log("You have a C or greater");
+
+  //let animalName="shrimp";
+  //let numberOfAnimal=1;
+  //if (numberOfAnimal > 1){
+    //console.log(numberOfAnimal +" " +  animalName + "s")
+  //}else {
+//console.log(numberOfAnimal + " " + animalName)
+  //}
+
+  //let number = 7
+  //if (number % 2 === 0) {
+    //console.log('Even number');
+//}else{
+  //console.log("Odd number")
+//}
+
+//let number = 7
+//number % 2 === 0 ? console.log('Even number') : console.log("Odd number")
+
+//let number = 5
+//if (number > 0) {
+  //console.log("number is positive");
+//} else if (number <0) {
+  //console.log("number is negative");
+//}else {
+  //console.log("number is 0");
+//}
