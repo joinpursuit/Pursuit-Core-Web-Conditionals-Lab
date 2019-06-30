ca059ead7f594f558e49422cea93aa906587fd25
Johanne <johanneenama@gmail.com>
2019-06-30 18:20:10 -0400

    conditional_assignment.js

---
+++ conditional_assignment/conditional_assignment.js
@@ -0,0 +1,121 @@
+// let x = 10;
+//  if (x >= 20) {
+//    console.log( 'then it is equal.' )
+//  } else {
+//    console.log('it is not equal')
+//  }
+
+
+// let num = 10
+// let num2 = 10
+// if (num > num2){
+//   console.log(num + ' is greater than ' + num2)
+// } else if (num === num2) {
+//   console.log('then the numbers are equal')
+// }else {
+// console.log(num + ' is less than ' + num2)
+// }
+
+
+// let language = "French";
+// if (language === "Spanish") {
+//   console.log('Hola Mundo')
+// } else if (language === "French") {
+//   console.log('Salut Monde')
+// } else if (language === "Turkish") {
+//   console.log('Selam Dunya');
+// } else {
+//   console.log('hello world');
+// }
+
+
+// let language = 'French'
+// switch (language) {
+//   case 'Turkish':
+//   console.log('Selam Dunya');
+//   break;
+//   case 'Spanish':
+//   case 'French':
+//     console.log('hola mundo' 'salut monde')
+//     break;
+//     default:
+//     console.log('hello world')
+// }
+
+
+
+// let numGrade = 97
+// if (numGrade >= 90) {
+//   console.log('A')
+// } else if (numGrade >= 80) {
+//   console.log('B')
+// } else if (numGrade >= 70) {
+//   console.log('C')
+// } else if ( numGrade >= 60) {
+//   console.log('D')
+// } else {
+//   console.log('F')
+
+
+
+// let animalName = 'dog'
+// let numberOfAnimals = '4'
+// if (numberOfAnimals >= 2) {
+// console.log('dogs')
+// } else {
+//   console.log('dog')
+// }
+
+//
+// let num = 50
+// if (num % 2 === 0) {
+//   console.log('even')
+// } else {
+//   console.log('odd')
+// }
+
+//
+// let num = 7
+// num % 2 === 0 ? console.log('even'): console.log('odd')
+
+
+// let num = 100
+// if ( num < 0) {
+//   console.log ("negative")
+// } else if (num = 0) {
+// console.log('equal to 0')
+// }else {
+//   console.log("positive")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
