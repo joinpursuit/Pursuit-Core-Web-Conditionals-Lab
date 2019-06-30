ab9bcd757c667c587465582047db972b8a68e31e
Suzette <suzetteislam@gmail.com>
2019-06-30 15:41:34 -0400

    modified version

--- conditional_assignment.js
+++ conditional_assignment.js
@@ -6,19 +6,19 @@
 //
 // 1. Write a block of code that checks and logs whether variables are equal or not.
 
-```
+
 let x = 33;
 if(x === 5){
   console.log("This is equal!")
 }else{
   console.log("This is not equal.")
 }
-```
+
 // 2. Write a block of code that logs which of two number variables is larger.
 //   * Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
 //   * If the two numbers are equal, log: 'The numbers are equal.'
 
-```
+
 let x = 4;
 let y = 4;
 if (x > y){
@@ -28,12 +28,12 @@ if (x > y){
 } else{
   console.log("The numbers are equal.")
 }
-```
+
 // 3. Create a string variable with a language code (e.g. "english", "spanish", "german").
 // Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages.
 // It should default to logging English.
 
-```
+
 let language = "Spanish";
 
 if(language === "English"){
@@ -45,11 +45,11 @@ if(language === "English"){
 } else{
   console.log("Hello, World.");
 }
-```
+
 
 // 4. Do question 3 again but this time use a `switch` statement.
 
-```
+
 let lang = "Spanish";
 switch(lang){
   case("English"):
@@ -64,12 +64,10 @@ switch(lang){
   default:
   console.log("Hello, World.");
 }
-```
 
 // 5. Create a variable numGrade that will contain a numeric grade (out of 100).
 // Use an if - else if ... - else statement to log the corresponding letter grade, either "A", "B", "C", "D", or "F".
 
-```
 let numGrade = 89
 if(numGrade >= 90 && numGrade <100){
   console.log("This is an'A'!");
@@ -82,17 +80,15 @@ if(numGrade >= 90 && numGrade <100){
 }else{
   console.log("Try again, this is a 'F'!");
 }
-```
 
 // 6. Create two variables: animalName ('cat', 'dog', etc.) , and numberOfAnimals.
 //
 // Use an if - else statement to log:
-//
+
 //   * the number
-//
+
 //   * either in single or plural form. Exp: "5 cats" or "1 dog".
 
-```
 let animalName = "dog";
 let numberOfAnimals = 1
 if (numberOfAnimals > 1){
@@ -100,28 +96,25 @@ if (numberOfAnimals > 1){
 } else {
   console.log(numberOfAnimals + " " + animalName)
 }
-```
+
 // Bonus: Make it handle a few collective nouns like "sheep" and "geese".
 
 // 7. Write a block of code that logs whether a given number is even or odd.
 
-```
 let num = 5;
 if (num % 2 === 0){
   console.log("The number is even.");
 } else{
   console.log("The number is odd.");
 }
-```
+
 // 8. Do question 7 again but use a ternary.
 
-```
 num % 2 === 0 ? console.log("The number is even") : console.log("The number is odd.");
-```
 
 // 9. Write a block of code that checks and logs whether a given number is positive, negative, or equal to 0.
 
-```
+
 let integer = -3;
 if (integer > 0){
   console.log("This number is positive.");
@@ -130,4 +123,3 @@ if (integer > 0){
 } else {
   console.log("This is a negative number.");
 }
-```
