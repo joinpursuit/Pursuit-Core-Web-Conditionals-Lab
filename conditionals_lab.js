c207a5ab558ab0f46690b01c218331a54274cde8
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-07-19 02:47:58 -0400

    v.2, renamed file for unified organization, stylistic revisions

--- conditionals_lab.js
+++ conditionals_lab.js
@@ -1,56 +1,32 @@
 // JOSEPH P. PASAOA
+//
 
-/*
-1. Write a block of code that checks and logs whether variables are equal or not.
 
-2. Write a block of code that logs which of two number variables is larger.
-  * Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
-  * If the two numbers are equal, log: 'The numbers are equal.'
 
-3. Create a string variable with a language code (e.g. "english", "spanish", "german").
-Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages.
-It should default to logging English.
+const spacer = () => console.log("");
+//
 
-4. Do question 3 again but this time use a `switch` statement.
 
-5. Create a variable numGrade that will contain a numeric grade (out of 100).
-Use an if - else if ... - else statement to log the corresponding letter grade, either "A", "B", "C", "D", or "F".
-
-6. Create two variables: animalName ('cat', 'dog', etc.) , and numberOfAnimals.
-
-Use an if - else statement to log:
-
-  * the number
-
-  * either in single or plural form. Exp: "5 cats" or "1 dog".
-
-Bonus: Make it handle a few collective nouns like "sheep" and "geese".
-
-7. Write a block of code that logs whether a given number is even or odd.
-
-8. Do question 7 again but use a ternary.
-
-9. Write a block of code that checks and logs whether a given number is positive, negative, or equal to 0.
-*/
-
+// 1
 console.log("1. Write a block of code that checks and logs whether variables are equal or not.");
 let TestVar1a = 900;
 let TestVar1b = 9000;
-//
+
 TestVar1a === TestVar1b ? console.log(TestVar1a + " and " + TestVar1b + " are equal.") : console.log(TestVar1a + " and " + TestVar1b + " are not equal.");
 //
-console.log("");
+spacer();
 
 
+// 2
 console.log("2. Write a block of code that logs which of two number variables is larger.");
 let TestVar2a = 800;
 let TestVar2b = 8000; // "An unwanted string";
-//
+
 let larger;
 if (TestVar2a > TestVar2b) {
   larger = TestVar2a;
   console.log("The greater number of " + TestVar2a + " and " + TestVar2b + " is " + larger + ".");
-} else if (TestVar2b > TestVar2a) {
+} else if (TestVar2a < TestVar2b) {
   larger = TestVar2b;
   console.log("The greater number of " + TestVar2a + " and " + TestVar2b + " is " + larger + ".");
 } else if (TestVar2a === TestVar2b) {
@@ -59,12 +35,13 @@ if (TestVar2a > TestVar2b) {
   console.log("Hmm. Something's fishy here. Please check your numbers and try again.");
 }
 //
-console.log("");
+spacer();
 
 
+// 3
 console.log('3. Create a string variable with a language code (e.g. "english", "spanish", "german").');
 let speaking = "Korean";
-//
+
 if (speaking === "English") {
   console.log("Hello, world");
 } else if (speaking === "Spanish") {
@@ -79,12 +56,13 @@ if (speaking === "English") {
   console.log("Sorry, I don't know that language.");
 }
 //
-console.log("");
+spacer();
 
 
+// 4
 console.log("4. Do question 3 again but this time use a `switch` statement.");
-//
 let talking = "Mongolian";
+
 switch (talking) {
   case "Spanish":
     console.log("Hola, mundo");
@@ -103,12 +81,13 @@ switch (talking) {
     break;
 }
 //
-console.log("");
+spacer();
 
 
+// 5
 console.log("5. Create a variable numGrade that will contain a numeric grade (out of 100).");
 let numGrade = 85;
-//
+
 if (numGrade >= 90) {
   console.log("A");
 } else if (numGrade >= 80) {
@@ -123,13 +102,14 @@ if (numGrade >= 90) {
   console.log("Please re-enter number grade.");
 }
 //
-console.log("");
+spacer();
 
 
+// 6
 console.log("6. Create two variables: animalName ('cat', 'dog', etc.) , and numberOfAnimals.");
 let animalName = "horse"; // HAS TO BE SINGULAR FORM INPUTED HERE
 let numberOfAnimals = 1;
-//
+
 if (animalName === "cat" || animalName === "dog" || animalName === "goose" || animalName === "sheep") {
   if (numberOfAnimals === 1) {
     console.log(numberOfAnimals + " " + animalName);
@@ -143,15 +123,16 @@ if (animalName === "cat" || animalName === "dog" || animalName === "goose" || an
     }
   }
 } else {
-  console.log("Sorry, I'll only accept the [cat], the [dog], the [goose], or the [sheep].");
+  console.log("Sorry, I'll only accept the [cat], the [dog], the [goose], or the [sheep], for now.");
 }
 //
-console.log("");
+spacer();
 
 
+// 7
 console.log("7. Write a block of code that logs whether a given number is even or odd.");
 let givenNum = 8.0; // 1.5;
-//
+
 let OOE = givenNum % 2;
 if (OOE === 0) {
   console.log(givenNum + " is even.");
@@ -161,12 +142,13 @@ if (OOE === 0) {
   console.log(givenNum + " is neither odd nor even.")
 }
 //
-console.log("");
+spacer();
 
 
+// 8
 console.log("8. Do question 7 again but use a ternary.");
 let anotherNum = 99.00; // 12.3;
-//
+
 let anotherOOE = anotherNum % 2;
 switch (anotherOOE) {
   case 0:
@@ -180,12 +162,13 @@ switch (anotherOOE) {
     break;
 }
 //
-console.log("");
+spacer();
 
 
+// 9
 console.log("9. Write a block of code that checks and logs whether a given number is positive, negative, or equal to 0.");
 let lastNum = -12345; // "not a number";
-//
+
 if (lastNum === 0) {
   console.log(lastNum + " is equal to 0. That is all.");
 } else if (lastNum > 0) {
