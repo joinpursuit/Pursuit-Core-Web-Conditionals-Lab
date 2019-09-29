a8a18d5f1378f5251be3bb6fa16b9d8064bf347f
Kelvin <kelvinarellano@pursuit.org>
2019-09-29 14:49:24 -0400

    first two problems

---
+++ Conditional.js
@@ -0,0 +1,10 @@
+// 1. Write a block of code that checks and logs whether variables are equal or not. 
+let a = 2
+let b = 1
+if (a === b) {
+    console.log("Variables are equal")
+}else {
+    console.log("They're not equal")
+}
+
+
