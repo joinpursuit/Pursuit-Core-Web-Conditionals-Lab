105226ad329e5558647cc21e0745dc57fd7065be
trilltown <phil.awich@gmail.com>
2019-09-29 15:27:07 -0400

    Problems 1-5 fin

--- conditionals.js
+++ conditionals.js
@@ -7,53 +7,6 @@ if (Pokemon === '150') {
     console.log("Keep trying");
 }
 
-// Problem 3
-
-
-let language = 'English';
-
-if (language = 'English') {
-    console.log('Hello, World')
-} else if (language = 'Spanish') {
-    console.log (Hola, Mundo) 
-} else (language = 'German') {
-    console.log (Hallo, Welt)
-}
-
-// > *Note*: Remember to fork this repo, clone your copy, modify `conditionals.js` with your answers. Lastly `git add` your changes, `git commit` and `git push`
-
-// 1. Write a block of code that checks and logs whether variables are equal or not.
-
-// 2. Write a block of code that logs which of two number variables is larger.
-//   * Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
-//   * If the two numbers are equal, log: 'The numbers are equal.'
- 
-// 3. Create a string variable with a language code (e.g. "english", "spanish", "german"). 
-// Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages. 
-// It should default to logging English.
-
-// 4. Do question 3 again but this time use a `switch` statement. 
-
-// 5. Create a variable numGrade that will contain a numeric grade (out of 100). 
-// Use an if - else if ... - else statement to log the corresponding letter grade, either "A", "B", "C", "D", or "F".
-
-// 6. Create two variables: animalName ('cat', 'dog', etc.) , and numberOfAnimals.
-
-// Use an if - else statement to log:
-
-//   * the number
-
-//   * either in single or plural form. Exp: "5 cats" or "1 dog".
-
-// Bonus: Make it handle a few collective nouns like "sheep" and "geese".
-
-// 7. Write a block of code that logs whether a given number is even or odd.
-
-// 8. Do question 7 again but use a ternary. 
-
-// 9. Write a block of code that checks and logs whether a given number is positive, negative, or equal to 0.
-
-
 // This is problem #2
 
 let num1 = ""
@@ -69,12 +22,36 @@ if (num1 > num2) {
     console.log("Please input a number")
 }
 
+
+// Problem 3
+
+
+let language = 'Spanish';
+
+if (language === 'English') {
+    console.log('Hello, World')
+} else if (language === 'Spanish') {
+    console.log ('Hola, Mundo') 
+} else if (language === 'German') {
+    console.log ('Hallo, Welt')
+} else {
+    console.log ('Not a recognized language')
+}
+
+
+
 // This is Problem #5
 
-let numGrade = 55
+let numGrade = 76
 
 if (numGrade <= 50) {
     console.log("F")
-} else if (numGrade > 51 || numGrade <= 65) {
+} else if (numGrade > 51 && numGrade <= 65) {
     console.log("D")
-} else if (numGrade )
+} else if (numGrade > 65 && numGrade <= 75) {
+    console.log("C") 
+} else if (numGrade > 75 && numGrade <= 85) {
+    console.log("B")
+} else { 
+    console.log("A")
+}
