a4b6c52f18b262bff277871eb164ace6650fa959
dannyl <danilovlaurent@pursuit.org>
2019-09-29 15:57:45 -0400

    problem 5

--- conditionals.js
+++ conditionals.js
@@ -41,3 +41,20 @@ if (language === spanish) {
 
 
 // problem 5
+
+let numgrade = 77
+if (numgrade >= 90){
+    console.log("A");
+}
+else if (numgrade >= 80 && numgrade < 90){
+    console.log("B");
+}
+else if (numgrade >= 70 && numgrade < 80){
+    console.log("C");
+}
+else if (numgrade >= 60 && numgrade< 70){
+    console.log("D");
+}
+else {
+    console.log("F");
+}
