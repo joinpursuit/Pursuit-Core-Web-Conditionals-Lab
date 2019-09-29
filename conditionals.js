fc8a1efaf7295d4ed7ceb917529cac5d944cb541
Oblivion <kiaraedwards@pursuit.org>
2019-09-29 14:02:40 -0400

    finished the first 3 don't know if the third one is right though.

---
+++ conditionals.js
@@ -0,0 +1,38 @@
+let high = 5
+let low = 2
+
+high === low ? console.log("equals") : console.log("not equals");
+
+
+let num = 20
+let num2 = 48
+
+num <= num2 ? console.log("10 is the greater number") : console.log("5 is the lower numnber");
+
+
+let string = "hello world"
+
+if (string !=  "hello world"){
+    console.log("hello world");
+} else if (string == "hello world"){
+    console.log("Привет");
+}else if (string !== "hello world"){
+    console.log("hola");
+} else{
+    console.log("done");
+}
+
+
+
+//  switch (new language().getlanguage()) {
+//     case 0:
+//       language = "Привет, мир";
+//       break;
+//     case 1:
+//       language = "Hola Mundo";
+//       break;
+//     case 2:
+//        language= "hello world";
+//       break;
+    
+//   }
