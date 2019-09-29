fb3e9cf557a763a0830f72944732839b0346fec8
Jovanni <jovanniluna@pursuit>
2019-09-29 14:22:36 -0400

    Problem one solved

---
+++ conditionals.js
@@ -0,0 +1,7 @@
+let numOne = 20
+let numTwo = 25
+if (numOne === numTwo){
+    console.log("Variables are equal to each other")
+} else {
+    console.log("Variables are not equal to each other")
+}
