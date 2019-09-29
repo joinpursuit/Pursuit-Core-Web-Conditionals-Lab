aa18fc1919dc62db429eb54dbdef0a2c63c58807
Henrynunez112 <henrynunez@pursuit.org>
2019-09-29 15:00:33 -0400

    1 question done

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+let x = 2
+let y = 2
+
+if (x > y){
+    console.log("Sorry but " + y + " is not greater than " + x);
+}else if ( x === y){
+    console.log("Ok, both of these numbers are equal")
+}else {
+    console.log("Sorry but " + x + " is not greater than" + y);
+}
