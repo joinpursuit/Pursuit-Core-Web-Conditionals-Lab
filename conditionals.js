a19e858a23b267544612642215e12e991464f262
dannyl <danilovlaurent@pursuit.org>
2019-09-29 15:16:15 -0400

    problem 2

--- conditionals.js
+++ conditionals.js
@@ -6,3 +6,17 @@ if (sample > sample2) {
 } else {
     console.log("Less than");
 }
+
+// problem 2
+let x = 10
+let y= 5
+if (x < y) {
+    console.log("The greater number of " + x + " and " + y + " is " + y + ".");
+} else if (x > y) {
+    console.log("The greater number of " + x + " and " + y + " is " + x + ".");
+} else if (x === y)  {
+console.log("The numbers are equal");
+}
+  else {
+      console.log("done checking");
+  }
