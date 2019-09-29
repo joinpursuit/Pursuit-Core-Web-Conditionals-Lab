c3a7c4c7cd75dd4746d0df8eed91e98f06363f99
Kelvin <kelvinarellano@pursuit.org>
2019-09-29 15:04:37 -0400

    Second problem done

--- Conditional.js
+++ Conditional.js
@@ -1,10 +1,17 @@
-// 1. Write a block of code that checks and logs whether variables are equal or not. 
-let a = 2
-let b = 1
-if (a === b) {
-    console.log("Variables are equal")
-}else {
-    console.log("They're not equal")
-}
-
+// // 1. Write a block of code that checks and logs whether variables are equal or not. 
+// let a = 2
+// let b = 1
+// if (a === b) {
+//     console.log("Variables are equal")
+// }else {
+//     console.log("They're not equal")
+// }
+let x = 5
+let y = 10
+x > y ?console.log("The Greater number of " + x + " and " + y + " is " + x) : console.log("The Greater number of " + x + " and " + y + " is " + y)
+// if (x>y){
+//     console.log("The Greater number of " + x + " and " + y + " is " + x)
+// } else {
+//     console.log("The Greater number of " + x + " and " + y + " is " + y)
+// }
 
