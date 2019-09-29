27208615de27f9261dfc6eb056f197e199100a10
Jovanni <jovanniluna@pursuit>
2019-09-29 14:52:37 -0400

    Problem 3 solved

--- conditionals.js
+++ conditionals.js
@@ -8,7 +8,7 @@ if (numOne === numTwo){
     console.log("Variables are not equal to each other")
 }
 
-// Problem two
+// Problem Two
 
 let numX = 5
 let numY = 10
@@ -21,5 +21,15 @@ console.log ("The greater number of " + numX +" and " + numY +" is " +numX)
 console.log ("The greater number of " + numX +" and " + numY +" is " +numY)
 } else {
 console.log ("The numbers are equal")
+}
 
+//Problem 3
+
+let language = "English"
+if (language === "Spanish"){
+    console.log("Hola al mundo")
+} else if (language === "German"){
+    console.log("Hallo Welt")
+} else {
+    console.log("Hello World")
 }
