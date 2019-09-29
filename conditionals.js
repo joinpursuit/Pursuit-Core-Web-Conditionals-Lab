1e14efc035e52da272a89a70aefa3ba82e826e34
Kevin Brutus <kevinbrutus@pursuit.org>
2019-09-29 14:30:26 -0400

    problem 2 done

--- conditionals.js
+++ conditionals.js
@@ -1,15 +1,23 @@
 // 1. Write a block of code that checks and logs whether variables are equal or not.
-let num1 = 3;
-let num2 = 3;
-if (num1 === num2){
-    console.log("The numbers are equal");
-} else {
-    console.log("The numbers are not equal");
-}
+// let num1 = 3;
+// let num2 = 3;
+// if (num1 === num2){
+//     console.log("The numbers are equal");
+// } else {
+//     console.log("The numbers are not equal");
+// }
 // 2. Write a block of code that logs which of two number variables is larger.
 //   * Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
 //   * If the two numbers are equal, log: 'The numbers are equal.'
- 
+let x = 5;
+let y = 10;
+if (x > y){
+    console.log('The greater number of ' + x + " and " + y + " is " + x);
+} else if (x < y){
+    console.log('The greater number of ' + x + " and " + y + " is " + y);
+} else {
+    console.log('The numbers are equal.');
+}
 // 3. Create a string variable with a language code (e.g. "english", "spanish", "german"). 
 // Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages. 
 // It should default to logging English.
