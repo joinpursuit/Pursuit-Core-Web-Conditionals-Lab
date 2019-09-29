c144ab8a12e7438921fcb246fc8ea668db39813a
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-09-29 16:20:15 -0400

    newChange

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,6 @@
+//Problem 1
+
+
 let x = 2
 let y = 2
 
@@ -7,4 +10,31 @@ if (x > y){
     console.log("Ok, both of these numbers are equal")
 }else {
     console.log("Sorry but " + x + " is not greater than" + y);
+}
+
+
+//Problem 2 
+
+let A = 7
+let B = 3
+
+if (A > B) {
+  console.log("The greater number of " + A + " and " + B + " is " + A);
+}else if (A < B) {
+    console.log("The lesser numner of " + A + " and " + B + " is " + B);
+}else { 
+    console.log("Both numbers are equal");
+}
+
+//Problem 3
+
+let hey = hello
+let hello = english
+
+if (hey = hello){ 
+console.log("Hello World");
+} else if (hey = hola){
+    console.log("Hola, Mundo");
+} else if (hey = hallo) {
+    console.log("Hallo Welt");
 }
