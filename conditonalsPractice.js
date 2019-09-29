d3500aa8ac9902a714aa46aeb0cc6e9282409007
maria liza <mariamartinez@pursuit.org>
2019-09-29 12:55:03 -0400

    finish lab question 1

---
+++ conditonalsPractice.js
@@ -0,0 +1,10 @@
+//1. Write a block of code that checks and logs whether variables are equal or not.
+
+let var1 = 4;
+let var2 = 6;
+if (var1 === var2) {
+    console.log(var1 + " is equal to " + var2);
+} else {
+    console.log(var1 + " is not equal to " + var2);
+}
+
