62d2f45dacb9c69c5d3f2682b18429afbcc0e789
trilltown <phil.awich@gmail.com>
2019-09-29 15:08:38 -0400

    Conditials update

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+// Problem 1
+
+let Pokemon = '150';
+if (Pokemon === '150') {
+    console.log("You've caught them all");
+} else {
+    console.log("Keep trying");
+}
+
+// Problem 3
+
+
+let language = 'English';
+
+if (language = 'English') {
+    console.log('Hello, World')
+} else if (language = 'Spanish') {
+    console.log (Hola, Mundo) 
+} else (language = 'German') {
+    console.log (Hallo, Welt)
+}
