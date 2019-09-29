f718861b649634461234d0951da4ff593fb605d5
Kelvin <kelvinarellano@pursuit.org>
2019-09-29 15:14:32 -0400

    Third Problem Done

--- Conditional.js
+++ Conditional.js
@@ -6,12 +6,23 @@
 // }else {
 //     console.log("They're not equal")
 // }
-let x = 5
-let y = 10
-x > y ?console.log("The Greater number of " + x + " and " + y + " is " + x) : console.log("The Greater number of " + x + " and " + y + " is " + y)
+// let x = 5
+// let y = 10
+// x > y ?console.log("The Greater number of " + x + " and " + y + " is " + x) : console.log("The Greater number of " + x + " and " + y + " is " + y)
 // if (x>y){
 //     console.log("The Greater number of " + x + " and " + y + " is " + x)
 // } else {
 //     console.log("The Greater number of " + x + " and " + y + " is " + y)
 // }
 
+// 3. Create a string variable with a language code (e.g. "english", "spanish", "german"). 
+// Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages. 
+// It should default to logging English.
+// let language = "German"
+// if (language === "Spanish") {
+//     console.log("Hola Mundo")
+// } else if (language === "German"){
+// console.log("Hallo Welt")
+// }else{
+//     console.log("Hello World")
+// }
