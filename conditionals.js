89d41f92dec4b3bc1753a921353d89f6f82ff726
Dugmar <dugmar.morocho@gmail.com>
2019-09-29 14:36:39 -0400

    Second Problem Set done

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,25 @@
+// Problem One
+
 let numOne = 20
 let numTwo = 25
 if (numOne === numTwo){
     console.log("Variables are equal to each other")
 } else {
     console.log("Variables are not equal to each other")
+}
+
+// Problem two
+
+let numX = 10
+let numY = 10
+
+if (numX > numY){
+console.log ("The greater number of " + numX +" and " + numY +" is " +numX)
+
+}else if (numY > numX){
+
+console.log ("The greater number of " + numX +" and " + numY +" is " +numY)
+} else {
+console.log ("The numbers are equal")
+
 }
