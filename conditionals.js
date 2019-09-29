18328b0d95c635799967d2a5a43077ed318a664e
Brandon Brown <brandonb0206@gmail.com>
2019-09-29 14:33:31 -0400

    Problem 2

--- conditionals.js
+++ conditionals.js
@@ -9,4 +9,15 @@ if(a === b){
     console.log("The variables aren't equal to eachother.");
 }
 
+//Problem 2
+let c = 5
+let d = 10
+
+if(c > d){
+    console.log("The greater number of " + c + " and " + d + " is " + c + ".");
+}else if(d > c){
+    console.log("The greater number of " + c + " and " + d + " is " + d + ".");
+}else{
+    console.log("The numbers are equal to eachother!");
+}
 
