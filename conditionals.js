05c9e03d3b39aeec50c2dc9b705465ae4b983ff4
Farrah Isabelle <farrahrios@pursuit.org>
2019-10-07 21:05:39 -0400

    sending conditinoal lab

---
+++ conditionals.js
@@ -0,0 +1,123 @@
+// // 1. Write a block of code that checks and logs whether variables are equal or not. 
+// // 2. Write a block of code that logs which of two number variables is larger.
+// // * Your output should look like this (in this example for the numbers x = 5 and y = 10): 'The greater number of 5 and 10 is 10'
+// // * If the two numbers are equal, log: 'The numbers are equal.'
+
+// let a = 7
+// let b = 4 
+
+
+if ( a === b ) {
+   console.log("We are the same!")
+} else  {
+    console.log("We are different")
+}
+
+let x = 9
+let y= 2
+if (x > y) {
+   console.log(x + " is the big dog")
+} else if (y>x) {
+   console.log( y + " is the big dog")
+} else {
+    console.log("We are both dogs of the same size")}
+
+
+
+
+
+//     var points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b){return b-a});
+
+
+
+
+
+
+//    // 3. Create a string variable with a language code (e.g. "english", "spanish", "german"). 
+// //Use an if - else if - else statement to log "Hello, World" in the given language, for at least 3 languages. 
+// //It should default to logging English.
+
+// //4. Do question 3 again but this time use a `switch` statement. 
+
+
+
+
+
+
+
+// // A =1
+// // B =2
+// // C =3
+
+
+// // If A is not bigger than B && A is not bigger than C 
+
+
+
+for (let num = 5; num < 100; num += 5) {
+    console.log(num);
+  }
+
+  let num = 5;
+while (num <= 100) {
+  console.log(num);
+  num += 5;
+}
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+
+{
+        console.log(sum);
+      }
+
+
+
+    //   let sum1 = 0;
+    //   for (let x = 0; x <100; x++){
+    //       console.log(x)
+    //       if (x % 3 === 0 && x % 5 === 0) {
+    //           console.log ('sum is' + sum1 )
+    //       }
+
+
+    //   } incomplete code 
+
+
+
+
+for (let i = 0; i < 10 ; i ++) {
+    if (i === 5){break;}
+    console.log(`I am looking at the number ${i}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
