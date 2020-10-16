2bd98fc4273a2acb4262e33a73798e235fc4c50b
Ben Stone <benjamin.h.stone@gmail.com>
2020-10-16 10:52:52 -0400

    update docs for conditionalsjs

--- problems/conditionals.js
+++ problems/conditionals.js
@@ -1,92 +1,77 @@
 /**
  * Takes in two values and returns if they are equal or not
- * @param {any} a - The first element.
- * @param {any} b - The second element.
+ * @param {*} a - The first element.
+ * @param {*} b - The second element.
  * @returns {boolean} - Is a equal to b.
  */
-function isEqual() {
+function isEqual() {}
 
-}
 /**
- 
-/**
- * Takes in two numbers and returns the larger number (numbers guaranteed to be of different value)
+ * Takes in two numbers and returns the larger number
+ * The numbers are guaranteed to be unique
  * @param {number} a - The first number.
  * @param {number} b - The second number.
- * @returns {number} - The larger number. 
+ * @returns {number} - The larger number.
  */
-function findLarger() {
+function findLarger() {}
 
-}
 /**
- 
-/**
- * Takes in two numbers and returns the larger number or the string "tie" in the event that the numbers are the same size.
+ * Takes in two numbers and returns the larger number
+ * Returns the string "tie" if both numbers are the same
  * @param {number} a - The first number.
  * @param {number} b - The second number.
- * @returns {number || string } - The larger number or string "tie" 
+ * @returns {number|string } - The larger number or string "tie"
  */
-function findLargerOrTie() {
+function findLargerOrTie() {}
 
-}
 /**
- 
-/**
- * Takes in a number and returns whether that number is positive,
- * negative, or equal to 0.
+ * Takes in a number and returns whether that number is positive, negative, or equal to 0.
  * @param {number} a - The number.
- * @returns {string} - "Positive, Negative, "Zero"" 
+ * @returns {string} - "Positive, Negative, "Zero""
  */
-function positiveNegativeOrZero() {
-
-}
-/**
- 
- 
- 
-/**
- * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language.
- * Must work for at least english, spanish, and german.
- *  It should default to English if no argument is passed in.
- * @param {string} language - The language.
- * @returns {string} - "Hello World" in the desired language
- */
-function aroundTheWorldGreeting() {
-
-}
-/**
- 
+function positiveNegativeOrZero() {}
 
 /**
  * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language.
  * Must work for at least english, spanish, and german.
- *  It should default to English if no argument is passed in.
- * Must be written using a SWITCH statement
+ * It should default to English if no argument is passed in.
+ * Translations: English -> "Hello World"
+ *               Spanish -> "Hola Mundo"
+ *               German -> "Hallo Welt"
  * @param {string} language - The language.
  * @returns {string} - "Hello World" in the desired language
  */
-function aroundTheWorldGreetingWithSwitch() {
+function aroundTheWorldGreeting() {}
 
-}
 /**
-
+ * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language.
+ * Must work for at least english, spanish, and german.
+ * It should default to English if no argument is passed in.
+ * Translations: English -> "Hello World"
+ *               Spanish -> "Hola Mundo"
+ *               German -> "Hallo Welt"
+ * Uses a switch statement in its implementation
+ * @param {string} language - The language.
+ * @returns {string} - "Hello World" in the desired language
+ */
+function aroundTheWorldGreetingWithSwitch() {}
 
 /**
  * Takes in a number and returns the letter grade corresponding to that number.
  * Letter grades consist of "A", "B", "C", "D", or "F"
- * 90 and above is an A, 80 and above is a B, etc...
+ * 90 and above is an A
+ * 80 and above is a B
+ * 70 and above is a C
+ * 60 and above is a D
+ * Anything lower than 60 is an F
  * @param {number} grade - The numeric grade.
  * @returns {string} - The letter grade corresponding to the numeric grade.
  */
-function calculateLetterGrade() {
-
-}
-/**
- 
+function calculateLetterGrade() {}
 
 /**
- * Takes in two variables: animal ('cat', 'dog', etc.) , and numberOfAnimals.
- * Use an if - else to statement to return the 
+ * Takes in two variables: animal ('cat', 'dog', etc.) and numberOfAnimals.
+ * Use an if - else to statement to return the
  * number and animal (in either single or plural form)
  * Exp: "5 cats" or "1 dog"
  * Bonus: Make it handle a few collecting nouns like "sheep" and "geese"
@@ -94,42 +79,34 @@ function calculateLetterGrade() {
  * @param {number} numberOfAnimals - The number of animals.
  * @returns {string} - the number and animal (in either single or plural form)
  */
-function animalCounts() {
+function animalCounts() {}
 
-}
-/**
- 
 
 /**
  * Takes in a number and returns whether that number is even or odd
  * @param {number} a - The number.
- * @returns {string} - "Even or Odd" 
+ * @returns {string} - "Even or Odd"
  */
-function evenOrOdd() {
+function evenOrOdd() {}
 
-}
-/**
- 
 /**
  * Takes in a number and returns whether that number is even or odd
- * Must use a ternary
+ * Uses a ternary in its implementation
  * @param {number} a - The number.
- * @returns {string} - "Even or Odd" 
+ * @returns {string} - "Even or Odd"
  */
-function evenOrOddWithTernary() {
+function evenOrOddWithTernary() {}
 
-}
- 
 /**
- * Takes in two numbers and returns the larger number (numbers guaranteed to be of different value)
+ * Takes in two numbers and returns the larger number
+ * The numbers are guaranteed to be unique
+ * Uses a ternary in its implementation
  * @param {number} a - The first number.
  * @param {number} b - The second number.
- * @returns {number} - The larger number. 
+ * @returns {number} - The larger number.
  */
-function findLargerTernary() {
+function findLargerTernary() {}
 
-}
- 
 
 
 module.exports = {
