df59068ec989fc5f14f3edf36f50b54b9d3c48cd
joshuamendez <joshuafmendez@gmail.com>
2020-12-09 18:55:15 -0500

    completed most problems in file still need to do last problem

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -4,7 +4,31 @@
  * @param {string} color - The color.
  * @returns {boolean} - Whether the color is a primary color
  */
-function isPrimaryColor() {}
+function isPrimaryColor(color) {
+  if(color === 'red'){
+    return true;
+  }else if(color === 'yellow'){
+    return true;
+  }else if(color === 'blue'){
+    return true;
+  }else{
+    return false;
+  }
+  //I spent so much time looking at terminal to find whats wrong an I was reading the wrong function output.
+  // switch(color){
+  //   case 'red':
+  //     return true;
+  //     break;
+  //   case 'yellow':
+  //     return true;
+  //     break;
+  //   case 'blue':
+  //     return true;
+  //     break;
+  //   default:
+  //     return false;
+  // }
+}
 
 /**
  * Takes in color and returns whether or not the color is a primary color.
@@ -12,7 +36,13 @@ function isPrimaryColor() {}
  * @param {string} color - The color.
  * @returns {boolean} - If primary color or not
  */
-function isPrimaryColorOneIf() {}
+function isPrimaryColorOneIf(color) {
+  if(color === 'red' || color === 'yellow' || color === 'blue'){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 /**
  * Takes in two words and returns the word that comes first in lexicographical order.
@@ -20,7 +50,14 @@ function isPrimaryColorOneIf() {}
  * @param {string} word2 - The second word.
  * @returns {string} - The word that is smaller in lexicographical order
  */
-function lexicographicalOrder() {}
+function lexicographicalOrder(word1, word2) {
+  // if(word1 < word2){
+  //   return word1;
+  // }else{
+  //   return word2;
+  // }
+  return word1 < word2 ? word1 : word2;
+}
 
 /**
  * Takes in a word and returns "large" if its length is 5 character or more.
@@ -28,7 +65,13 @@ function lexicographicalOrder() {}
  * @param {string} word
  * @returns {string} - "large" or "small"
  */
-function largeOrSmall() {}
+function largeOrSmall(word) {
+  if(word.length >= 5){
+    return 'large';
+  }else{
+    return 'small';
+  }
+}
 
 /**
  * Takes in two words and returns the longer word.
@@ -36,7 +79,13 @@ function largeOrSmall() {}
  * @param {string} word2 - The second word.
  * @returns {string} - The longer word
  */
-function longerWord() {}
+function longerWord(word1, word2) {
+  if(word1.length >= word2.length){
+    return word1;
+  }else{
+    return word2;
+  }
+}
 
 /**
  * Takes in three words and returns the shortest word.
@@ -45,7 +94,9 @@ function longerWord() {}
  * @param {string} word3 - The second word.
  * @returns {string} - The shortest word
  */
-function shortestWord() {}
+function shortestWord(word1, word2, word3) {
+  //will do after class
+}
 
 module.exports = {
   isPrimaryColor,
