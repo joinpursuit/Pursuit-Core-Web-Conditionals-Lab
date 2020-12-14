a500b1d7dab151f61e902967e488559b95157a97
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2020-12-14 16:47:09 -0500

    first attempt at problems

--- problems/conditionals.js
+++ problems/conditionals.js
@@ -4,7 +4,9 @@
  * @param {*} b - The second element.
  * @returns {boolean} - Is a equal to b.
  */
-function isEqual() {}
+function isEqual(a , b) {
+  return a === b 
+}
 
 /**
  * Takes in two numbers and returns the larger number
@@ -13,7 +15,16 @@ function isEqual() {}
  * @param {number} b - The second number.
  * @returns {number} - The larger number.
  */
-function findLarger() {}
+// a function that takes in two numbers; 
+// a function that returns the larger number
+function findLarger(a , b) {
+  // return Math.max(a , b) 
+  if (a > b) {
+    return a}
+  else {
+    return b
+  }
+}
 
 /**
  * Takes in two numbers and returns the larger number
@@ -22,14 +33,37 @@ function findLarger() {}
  * @param {number} b - The second number.
  * @returns {number|string } - The larger number or string "tie"
  */
-function findLargerOrTie() {}
+function findLargerOrTie(a , b) {
+  if (a > b) {
+    return a
+  }
+  else if (a == b) {
+    return "tie"
+  }
+  else {
+    return b 
+  }
+}
+
+
 
 /**
  * Takes in a number and returns whether that number is positive, negative, or equal to 0.
  * @param {number} a - The number.
  * @returns {string} - "Positive, Negative, "Zero""
  */
-function positiveNegativeOrZero() {}
+function positiveNegativeOrZero(a) {
+  if (a > 0) {
+  return "Positive" 
+  }
+  else if (a < 0) {
+    return "Negative"
+  }
+  else {
+    return "Zero"
+  }
+}
+
 
 /**
  * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language.
@@ -37,11 +71,24 @@ function positiveNegativeOrZero() {}
  * It should default to English if no argument is passed in.
  * Translations: English -> "Hello World"
  *               Spanish -> "Hola Mundo"
- *               German -> "Hallo Welt"
+ *               German -> 
  * @param {string} language - The language.
  * @returns {string} - "Hello World" in the desired language
  */
-function aroundTheWorldGreeting() {}
+function aroundTheWorldGreeting(language) {
+  if (language === "english") {
+    return "Hello World"
+  }
+  else if (language === "spanish") {
+    return "Hola Mundo"
+  }
+  else if (language === "german") {
+    return "Hallo Welt"
+  }
+  else {
+    return "Hello World"
+  }
+}
 
 /**
  * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language.
