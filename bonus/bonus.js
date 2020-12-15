6e53afdef79b387c5708017ccba48e0df84626e6
vrwatson <vanessawatson@pursuit.org>
2020-12-14 21:21:46 -0500

    last 3 bonus problems

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -46,7 +46,13 @@ if(word1 < word2){
  * @param {string} word
  * @returns {string} - "large" or "small"
  */
-function largeOrSmall() {}
+const largeOrSmall = (word) => {
+  if (word.length >= 5) {
+    return "large"
+  } else {
+    return "small"
+  }
+}
 
 /**
  * Takes in two words and returns the longer word.
@@ -54,17 +60,31 @@ function largeOrSmall() {}
  * @param {string} word2 - The second word.
  * @returns {string} - The longer word
  */
-function longerWord() {}
+const longerWord = (word1, word2) => {
+  return word1.length > word2.length ? word1 : word2
+}
 
 /**
  * Takes in three words and returns the shortest word.
  * @param {string} word1 - The first word.
  * @param {string} word2 - The second word.
- * @param {string} word3 - The second word.
+ * @param {string} word3 - The second word.  **(FYI, second word twice instead of third word)**
  * @returns {string} - The shortest word
  */
-function shortestWord() {}
+const shortestWord = (word1, word2, word3) => {
+  let shortestWord = Math.min(word1.length, word2.length, word3.length); 
+    if (word1.length === shortestWord) {
+      return word1
+      }
+      else if (word2.length === shortestWord) {
+        return word2
+      }
+      else if (word3.length === shortestWord) {
+        return word3
+      }
+  }
 
+  
 module.exports = {
   isPrimaryColor,
   isPrimaryColorOneIf,
